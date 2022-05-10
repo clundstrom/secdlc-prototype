c55fc452714f0b7306205168033bbd2fea3a674e
Elias Elfving <elias.elfving@mail.com>
2022-05-10 09:22:10 +0200

    Added static response to admin:admin login, added schema verification for post requests

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const lib = require('./lib')
 const app = new express();
-
+const SCHEMAS = require('./schemas')
 
 let port =  process.env.PORT | 2022
 let host = process.env.HOST | "127.0.0.1"
@@ -12,45 +12,67 @@ let host = process.env.HOST | "127.0.0.1"
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Nothing to see here' + lib.validateToken("aa"))
-});
-
-app.get('/test', (req, res) => {
-    res.cookie("test", "value")
-        .send('Hello this is API test');
-});
-
-app.post('/testpost', (req, res) => {
-    console.log(req.body)
-    res.send('Test post')
+    res.send('Nothing to see here ')
 });
 
 app.post('/login', (req, res) => {
-    res.send('You are now logged in');
+    lib.verifySchema(req.body, SCHEMAS.loginSchema, (result, errors) => {
+        if(result){
+            if(req.body.username === process.env.ADMIN_USER && req.body.password === process.env.ADMIN_PASS){
+                res.status(200).cookie("webToken", process.env.ROOT).send("Login successful!")
+            } else {
+                res.status(401).send('Invalid credentials')
+            }
+        } else {
+            console.log(new Date(), ": ", JSON.stringify(errors));
+            res.status(400).send("Invalid post parameters");
+        }
+    });
 });
 
 app.post('/logout', (req, res) => {
-    res.send('You are now logged out');
+    res.status(200).send('You are now logged out');
 });
 
-app.post('/creatUser', (req, res) => {
-    res.send("You have created a user");
+app.post('/createUser', (req, res) => {
+    res.status(201).send("You have created a user");
 });
 
 app.get('/getInventory', (req, res) => {
-    res.send(require('./sample_db_answer.json'));
+    res.status(200).send(require('./sample_db_answer.json'));
 });
 
 app.post('/addItem', (req, res) => {
-    res.send('You have added an item ' );
+    lib.verifySchema(req.body, SCHEMAS.addItemSchema, (result, errors) => {
+        if(result){
+            res.status(201).send("Item added successfuly succesful")
+        } else {
+            console.log(new Date(), ": ", JSON.stringify(errors));
+            res.status(400).send("Invalid post parameters");
+        }
+    });
 });
 
 app.post('/removeItem', (req, res) => {
-    res.send('You have removed an item');
+    lib.verifySchema(req.body, SCHEMAS.removeItemSchema, (result, errors) => {
+        if(result){
+            res.status(200).send("Removal succesful")
+        } else {
+            console.log(new Date(), ": ", JSON.stringify(errors));
+            res.status(400).send("Invalid post parameters");
+        }
+    });
 });
 
 app.post('/updateitem', (req, res) => {
-    res.send('You have updated an item');
+    lib.verifySchema(req.body, SCHEMAS.updateItemSchema, (result, errors) => {
+        if(result){
+            res.status(200).send("Update succesful")
+        } else {
+            console.log(new Date(), ": ", JSON.stringify(errors));
+            res.status(400).send("Invalid post parameters");
+        }
+    });
 });
 
 app.listen(port, host, () => {
