25638bf550c4ae8dc4ad7d9235605bf14bcad054
Elias Elfving <elias.elfving@mail.com>
2022-05-10 11:29:29 +0200

    Updated database

--- database/tables.sql
+++ database/tables.sql
@@ -5,9 +5,8 @@ USE authio;
 CREATE TABLE IF NOT EXISTS `users` (
     `id` int(10) NOT NULL AUTO_INCREMENT,
     `name` varchar(50) NOT NULL,
-    `sha2` varchar(128) DEFAULT NULL,
-    `salt` varchar(128) DEFAULT NULL,
-    `privilege_level` int(10) NOT NULL,
+    `hash` varchar(60),
+    `access` varchar(50) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE (`name`),
     INDEX (`name`)
@@ -17,7 +16,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 CREATE TABLE IF NOT EXISTS `inventory` (
     `id` int(10) NOT NULL  AUTO_INCREMENT,
     `name` varchar(50) DEFAULT NULL,
-    `count` varchar(128) DEFAULT NULL,
+    `quantity` varchar(128) DEFAULT NULL,
+    `type` int(10) DEFAULT NULL,
     `description` varchar(128) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE (`name`),
