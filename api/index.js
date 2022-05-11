79f375d7a20b10d14fc8f74e1c428a206fec30b7
Elias Elfving <elias.elfving@mail.com>
2022-05-11 20:02:44 +0200

    Inventory get works, should still look HTTP responses

--- api/index.js
+++ api/index.js
@@ -5,14 +5,15 @@ const lib = require('./lib')
 const bcrypt = require('bcrypt')
 const mariadb = require('mariadb')
 const app = new express();
-const SCHEMAS = require('./schemas')
-const port =  process.env.PORT || 2022
+const SCHEMAS = require('./schemas');
+const { verify } = require('jsonwebtoken');
+const port = process.env.PORT || 2022
 const host = process.env.HOST || "127.0.0.1"
 const DB_USER = process.env.DB_USER || "authio_service_account"
 const DB_HOST = process.env.DB_HOST || "127.0.0.1"
 const DB_PASS = process.env.DB_PASS || "<your_mysql_password>"
 console.log(DB_USER, DB_HOST, DB_PASS)
-const pool = mariadb.createPool({host: DB_HOST, user: DB_USER, password: DB_PASS, database: "authio", connectionLimit: 5});
+const pool = mariadb.createPool({ host: DB_HOST, user: DB_USER, password: DB_PASS, database: "authio", connectionLimit: 5 });
 
 app.use(express.json())
 
@@ -21,42 +22,42 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    const {username, password} = req.body
-    const hash = 
-    lib.verifySchema(req.body, SCHEMAS.loginSchema, (result, errors) => {
-        if(result){
-            
-            pool.getConnection()
-                .then(conn => {
-                    conn.query(`SELECT hash, access FROM users WHERE name = ?`, [username])
-                    .then((rows) => {
-                        if(rows.length !== 1){
-                            res.status(401).send("Invalid credentials")
-                            conn.end();
-                            return
-                        }
-                        let hash = rows[0].hash
-                        let access = rows[0].access
-                        bcrypt.compare(password, hash, (error, result) => {
-                            if(result){
-                                let token = lib.generateToken({user:username, access:access})
-                                console.log("Generated token: ", token)
-                                res.status(200).cookie("webToken", token).send("Login successful")
-                            } else {
-                                res.status(401).send("Invalid credentials")
-                            }
-                        })
-                        conn.end();
-                    })    
-                }).catch(err => {
-                    //not connected
-                    res.status(401).send(err)
-                });
-        } else {
-            console.log(new Date(), ": ", JSON.stringify(errors));
-            res.status(400).send("Invalid post parameters");
-        }
-    });
+    const { username, password } = req.body
+    const hash =
+        lib.verifySchema(req.body, SCHEMAS.loginSchema, (result, errors) => {
+            if (result) {
+
+                pool.getConnection()
+                    .then(conn => {
+                        conn.query(`SELECT hash, access FROM users WHERE name = ?`, [username])
+                            .then((rows) => {
+                                if (rows.length !== 1) {
+                                    res.status(401).send("Invalid credentials")
+                                    conn.end();
+                                    return
+                                }
+                                let hash = rows[0].hash
+                                let access = rows[0].access
+                                bcrypt.compare(password, hash, (error, result) => {
+                                    if (result) {
+                                        let token = lib.generateToken({ user: username, access: access })
+                                        console.log("Generated token: ", token)
+                                        res.status(200).cookie("webToken", token).send("Login successful")
+                                    } else {
+                                        res.status(401).send("Invalid credentials")
+                                    }
+                                })
+                                conn.end();
+                            })
+                    }).catch(err => {
+                        //not connected
+                        res.status(401).send(err)
+                    });
+            } else {
+                console.log(new Date(), ": ", JSON.stringify(errors));
+                res.status(400).send("Invalid post parameters");
+            }
+        });
 });
 
 app.post('/logout', (req, res) => {
@@ -68,12 +69,32 @@ app.post('/createUser', (req, res) => {
 });
 
 app.get('/getInventory', (req, res) => {
-    res.status(200).send(require('./sample_db_answer.json'));
+    var token = lib.getToken(req)
+    var {err, result} = lib.verifyToken(token)
+    console.log(err, result)
+    if(!err){
+        pool.getConnection()
+        .then(conn => {
+            console.log(result)
+            let access = result.access;
+            conn.query(`SELECT name, quantity, type, description FROM inventory WHERE type IN (${access})`)
+                .then((rows) => {
+                    console.log(rows)
+                    res.status(200).json(rows).send()
+                    conn.end();
+                })
+        }).catch(err => {
+            //not connected
+            res.status(401).send(err)
+        });
+    } else {
+        res.status(401).send(result)
+    }
 });
 
 app.post('/addItem', (req, res) => {
     lib.verifySchema(req.body, SCHEMAS.addItemSchema, (result, errors) => {
-        if(result){
+        if (result) {
             res.status(201).send("Item added successfuly succesful")
         } else {
             console.log(new Date(), ": ", JSON.stringify(errors));
@@ -84,7 +105,7 @@ app.post('/addItem', (req, res) => {
 
 app.post('/removeItem', (req, res) => {
     lib.verifySchema(req.body, SCHEMAS.removeItemSchema, (result, errors) => {
-        if(result){
+        if (result) {
             res.status(200).send("Removal succesful")
         } else {
             console.log(new Date(), ": ", JSON.stringify(errors));
@@ -95,7 +116,7 @@ app.post('/removeItem', (req, res) => {
 
 app.post('/updateitem', (req, res) => {
     lib.verifySchema(req.body, SCHEMAS.updateItemSchema, (result, errors) => {
-        if(result){
+        if (result) {
             res.status(200).send("Update succesful")
         } else {
             console.log(new Date(), ": ", JSON.stringify(errors));
--- api/lib.js
+++ api/lib.js
@@ -13,13 +13,24 @@ function generateToken(payload){
   return jwt.sign(payload, priv, { algorithm: 'RS256'})
 }
 
+function verifyToken(payload){
+  var pub = Buffer.from(process.env.PUB, 'base64').toString('ascii')
+  try{
+    var body = jwt.verify(payload, pub, { algorithm: 'RS256'})
+  } catch(err){
+    console.log(new Date(), ": ", err.name, err.message)
+    return {"err":true, "result":{...err.name,...err.message}}
+  }
+  return {"err":false, "result":body}
+}
+
 function verifySchema(payload, schema, callback) {
   const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
   const validate = ajv.compile(schema);
   const valid = validate(payload);
   callback(valid, validate.errors)
 }
-
+module.exports.verifyToken = verifyToken
 module.exports.generateToken = generateToken
 module.exports.getToken = getToken
 module.exports.verifySchema = verifySchema
