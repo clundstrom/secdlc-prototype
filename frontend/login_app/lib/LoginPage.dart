19cf14ce975ca9e207726696ca46ed81568a0062
jakobodman123 <jakobodman1997@gmail.com>
2022-05-12 23:05:48 +0200

    fungerande login

--- frontend/login_app/lib/LoginPage.dart
+++ frontend/login_app/lib/LoginPage.dart
@@ -1,8 +1,12 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:login_app/signUpPage.dart';
 import 'package:login_app/testPage.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
 
 class LoginPage extends StatefulWidget {
   const LoginPage();
@@ -47,20 +51,106 @@ class _SignInFormState extends State<LoginPage> {
   String currentTextUsername = "";
   String currentTextPW = "";
 
-  Future<void> login(String username, String password) async {
-    final response = await http
-        .post(Uri.parse('http://localhost:2022/login'), //något liknande här
-            body: {"username": username, "password": password});
+  Future<int> login(String username, String password) async {
+    final msg = jsonEncode({
+      "message": username,
+      "password": password,
+    });
+
+    Map data = {"username": username, "password": password};
+
+    var body = json.encode(data);
+
+    final response = await http.post(
+      Uri.parse('http://localhost:2022/login'),
+      headers: {
+        HttpHeaders.contentTypeHeader: 'application/json',
+      }, //något liknande här
+      //body: {"username": "root","password": "root"}
+      //body: msg,
+      body: body,
+    );
     if (response.statusCode == 200) {
+      /*
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => const TestPage()),
       );
+      */
+      return response.statusCode;
     } else {
-      throw Exception('Unexpected error occured!');
+      /*
+      showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text("Error"),
+          actions: <Widget>[
+            TextButton(
+                onPressed: () => Navigator.pop(context, 'OK'),
+                child: const Text('OK')),
+          ],
+        ),
+      );
+      */
+      return response.statusCode;
     }
   }
 
+  Future<String?> attemptLogIn(String username, String password) async {
+    var res = await http.post(Uri.parse('http://localhost:2022/login'),
+        body: {"username": username, "password": password});
+    if (res.statusCode == 200) return res.body;
+    return null;
+  }
+
+  /*
+  final Dio _dio = Dio();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Future<Response> loginResponse(String email, String password) async {
+    try {
+      Response response = await _dio.post(
+        'http://localhost:2022/login',
+        data: {'email': email, 'password': password},
+      );
+      //returns the successful user data json object
+      return response.data;
+    } on DioError catch (e) {
+      //returns the error object if any
+      return e.response!.data;
+    }
+  }
+
+  Future<void> login() async {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text('Processing Data'),
+        backgroundColor: Colors.green.shade300,
+      ));
+
+      dynamic res = await loginResponse(
+        usernameTextController.text,
+        passwordTextController.text,
+      );
+
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+      if (res['ErrorCode'] == null) {
+        String accessToken = res['access_token'];
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                //builder: (context) => TestPage(accesstoken: accessToken)));
+                builder: (context) => TestPage()));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Error: ${res['Message']}'),
+          backgroundColor: Colors.red.shade300,
+        ));
+      }
+    }
+  }
+*/
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -135,7 +225,30 @@ class _SignInFormState extends State<LoginPage> {
                 minWidth: MediaQuery.of(context).size.width,
                 padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
                 onPressed: () async {
-                  await login(currentTextUsername, currentTextPW);
+                  String username = usernameTextController.text;
+                  String password = passwordTextController.text;
+                  var statuscode = await login(username, password);
+
+                  //if (statuscode == 200) {
+                  if (statuscode == 200) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const TestPage()),
+                    );
+                  } else {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: Text(
+                            username + password + " " + statuscode.toString()),
+                        actions: <Widget>[
+                          TextButton(
+                              onPressed: () => Navigator.pop(context, 'OK'),
+                              child: const Text('OK')),
+                        ],
+                      ),
+                    );
+                  }
                   //skicka data från textfields här
                   /*
                   if (1 == 1) {
